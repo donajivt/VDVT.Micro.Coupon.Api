8e84fa06fff8516b01bc2bef57d0fedb12906d6b
“donajivt” <vaniadonajitorres@gmail.com>
2025-06-03 15:56:08 -0600

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VDVT.Micro.Coupon.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Models.Coupon>().HasData(new Models.Coupon
+            {
+                CouponId = 1,
+                CouponCode = "100FF",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+            modelBuilder.Entity<Models.Coupon>().HasData(new Models.Coupon
+            {
+                CouponId = 2,
+                CouponCode = "200FF",
+                DiscountAmount = 20,
+                MinAmount = 40
+            });
+        }
+    }
+}
---
+++ Models/Dto/CouponDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VDVT.Micro.Coupon.Api.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public string AmountType { get; set; }
+        public int LimitUse { get; set; }
+        public DateTime DateInit { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Category { get; set; }
+        public bool StateCoupon { get; set; }
+    }
+}
